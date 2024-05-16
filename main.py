f9ac02d2c7973582eb434e2ce882882c2a66fe46
Artem <ishutinartem32@gmail.com>
2024-05-16 13:46:08 +0300

    added prevent for caching md images

--- main.py
+++ main.py
@@ -40,6 +40,17 @@ def generate_svg_endpoint():
     return Response(svg_image, mimetype='image/svg+xml', headers={
         "Content-Disposition": "attachment; filename=image.svg"
     })
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 port_number = 80
 if __name__ == '__main__':
     app.run(debug=False,host='0.0.0.0',port=port_number)
