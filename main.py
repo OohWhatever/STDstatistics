cc1de9ea341b1fd5d659a3870c0955aba35bf24b
OohWhatever <41408448+OohWhatever@users.noreply.github.com>
2024-05-18 03:39:02 +0300

    some magic with colors ans sizes

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def fetch_user_data(username):
 
 def generate_svg(username, position, crVulnerability, bbTotal, crBusinessRisk):
     svg_template = f'''
-  <svg xmlns="http://www.w3.org/2000/svg" width="470" height="180">
+   <svg xmlns="http://www.w3.org/2000/svg" width="470" height="180">
   <rect x="0" y="0" width="470" height="180" rx="20" ry="20" fill="url(#MyGradient)" />
 
     <text x="30" y="40" fill="white" font-size="25" font-weight="bold" font-family="Arial">
@@ -26,9 +26,9 @@ def generate_svg(username, position, crVulnerability, bbTotal, crBusinessRisk):
     Username: </text>
    <text x="110" y="80" fill="#00ff33" font-size="13" font-weight="bold" font-family="Arial">
     {username}</text>
-  <text x="75%" y="40%" dominant-baseline="middle" text-anchor="middle" fill="white" font-size="17" font-weight="bold" font-family="Arial">
+  <text x="75%" y="80" dominant-baseline="middle" text-anchor="middle" fill="white" font-size="17" font-weight="bold" font-family="Arial">
     CYBERRANGE RATING </text>
-   <text x="75%" y="65%" dominant-baseline="middle" text-anchor="middle" fill="#ff0000" font-size="70" font-weight="bold" font-family="Arial">
+   <text x="75%" y="73%" dominant-baseline="middle" text-anchor="middle" fill="#ff0000" font-size="70" font-weight="bold" font-family="Arial">
     {position}</text>
   <text x="30" y="102" fill="white" font-size="13" font-weight="bold" font-family="Arial">
     Vuln score: </text>
