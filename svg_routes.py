32422c1d3aa78cb06cf237fd3a06cebe376e104d
Artem <41408448+OohWhatever@users.noreply.github.com>
2024-09-05 21:58:35 +0300

    Fixed svg_routes.py

--- svg_routes.py
+++ svg_routes.py
@@ -87,11 +87,10 @@ def generate_svg(position, crPosition, bbPosition, username, total,bbTotal ,crTo
 def generatebb_svg(username, bbPosition,signal,impact,reportsTotal,reportsFinished,reportsInProgress,reportsAccepted,reportsInformational,reportsDuplicate,reportsOutOfScope,reportsRejected,reportsAbuse):
     bbsvg_template = f'''
 
-
-      <svg xmlns="http://www.w3.org/2000/svg" width="300" height="280" fill="none">
+<svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" fill="none">
     <rect width="220" height="20" y="48" x="10" rx="10" fill="url(#grad)" stroke="#FFFFFF" stroke-width="2"  />
          <rect width="220" height="20" y="68" x="10" rx="10" fill="url(#grad)" stroke="#FFFFFF" stroke-width="2"  />
-         <rect width="220" height="20" y="88" x="10" rx="10" fill="url(#grad)" stroke="#FFFFFF" stroke-width="2"  />
+         <rect width="220" height="20" y="88" x="10" rx="10" fill="url(#grad)" stroke="#FFFFFF" stroke-width="2"  />  
          <rect width="220" height="20" y="108" x="10" rx="10" fill="url(#grad)" stroke="#FFFFFF" stroke-width="2"  />
          <rect width="220" height="20" y="128" x="10" rx="10" fill="url(#grad)" stroke="#FFFFFF" stroke-width="2"  />
          <rect width="220" height="20" y="148" x="10" rx="10" fill="url(#grad)" stroke="#FFFFFF" stroke-width="2"  />
@@ -99,6 +98,8 @@ def generatebb_svg(username, bbPosition,signal,impact,reportsTotal,reportsFinish
            <rect width="220" height="20" y="188" x="10" rx="10" fill="url(#grad)" stroke="#FFFFFF" stroke-width="2"  />
            <rect width="220" height="20" y="208" x="10" rx="10" fill="url(#grad)" stroke="#FFFFFF" stroke-width="2"  />
            <rect width="220" height="20" y="228" x="10" rx="10" fill="url(#grad)" stroke="#FFFFFF" stroke-width="2"  />
+           <rect width="220" height="20" y="248" x="10" rx="10" fill="url(#grad)" stroke="#FFFFFF" stroke-width="2"  />
+           <rect width="220" height="20" y="268" x="10" rx="10" fill="url(#grad)" stroke="#FFFFFF" stroke-width="2"  />
            <defs>
     <linearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="0%">
       <stop offset="0%" style="stop-color: #4f0014; stop-opacity: 1" />
@@ -108,66 +109,73 @@ def generatebb_svg(username, bbPosition,signal,impact,reportsTotal,reportsFinish
   <polygon points="10,0 120,0 190,50 10,50" fill="#470736" rx="10" />
   <rect width="300" height="35" y="15" rx="10" fill="url(#grad)" stroke="#FFFFFF" stroke-width="2"  />
   <text x="10" y="38" fill="#f5f5f5" font-size="20" font-weight="bold" font-family="Arial">
-    Cyberrange Rating:</text>
+    BugBounty Rating:</text>
    <text x="292" y="41" fill="#88f000" text-anchor="end" font-size="28" font-weight="bold" font-family="Arial">
-    {crPosition}</text>
+    {bbPosition}</text>
     <text x="25" y="10" fill="#dbdbdb" font-size="11" font-weight="bold" font-family="Arial">
     STANDOFF365</text>
    <text x="25" y="63" fill="#dbdbdb" font-size="11" font-weight="bold" font-family="Arial">
     Username:  </text>
    <text x="25" y="83" fill="#dbdbdb" font-size="11" font-weight="bold" font-family="Arial">
-    Total rating:  </text>
+    Reports quality:  </text>
    <text x="25" y="103" fill="#dbdbdb" font-size="11" font-weight="bold" font-family="Arial">
-    BugBounty rating:  </text>
+    Reports impact:  </text>
    <text x="25" y="123" fill="#dbdbdb" font-size="11" font-weight="bold" font-family="Arial">
-    Tolal score:  </text>
+    Tolal report count:  </text>
    <text x="25" y="143" fill="#dbdbdb" font-size="11" font-weight="bold" font-family="Arial">
-    BugBounty score:  </text>
+    Reports finished:  </text>
    <text x="25" y="163" fill="#dbdbdb" font-size="11" font-weight="bold" font-family="Arial">
-    Cyberrange score:  </text>
+    Reports in progress:  </text>
    <text x="25" y="183" fill="#dbdbdb" font-size="11" font-weight="bold" font-family="Arial">
-    BugBounty report count:  </text>
+    Accepted reports:  </text>
    <text x="25" y="203" fill="#dbdbdb" font-size="11" font-weight="bold" font-family="Arial">
-    Cyberrange report count:  </text>
+    Informational:  </text>
    <text x="25" y="223" fill="#dbdbdb" font-size="11" font-weight="bold" font-family="Arial">
-    Vulnerability score:  </text>
+    Duplicate:  </text>
    <text x="25" y="243" fill="#dbdbdb" font-size="11" font-weight="bold" font-family="Arial">
-    Business Risk score:  </text>
-
+    Out of scope:  </text>
+   <text x="25" y="263" fill="#dbdbdb" font-size="11" font-weight="bold" font-family="Arial">
+    Rejected:  </text>
+   <text x="25" y="283" fill="#dbdbdb" font-size="11" font-weight="bold" font-family="Arial">
+    Spam:  </text>
  <text x="220" y="63" text-anchor="end" fill="#00ffa6" font-size="13" font-weight="bold" font-family="Arial">
   {username}
 </text>
  <text x="220" y="83" text-anchor="end" fill="#00ffa6" font-size="13" font-weight="bold" font-family="Arial">
-  {position}
+  {signal}
 </text>
-     
+
  <text x="220" y="103" text-anchor="end" fill="#00ffa6" font-size="13" font-weight="bold" font-family="Arial">
-  {bbPosition}
+  {impact}
 </text>
  <text x="220" y="123" text-anchor="end" fill="#00ffa6" font-size="13" font-weight="bold" font-family="Arial">
-  {total}
-</text>
- <text x="220" y="143" text-anchor="end" fill="#00ffa6" font-size="13" font-weight="bold" font-family="Arial">
-  {bbTotal}
-</text>
- <text x="220" y="163" text-anchor="end" fill="#00ffa6" font-size="13" font-weight="bold" font-family="Arial">
-  {crTotal}
-</text>
- <text x="220" y="183" text-anchor="end" fill="#00ffa6" font-size="13" font-weight="bold" font-family="Arial">
   {bbReportsCount}
 </text>
+ <text x="220" y="143" text-anchor="end" fill="#00ffa6" font-size="13" font-weight="bold" font-family="Arial">
+  {reportsFinished}
+</text>
+ <text x="220" y="163" text-anchor="end" fill="#00ffa6" font-size="13" font-weight="bold" font-family="Arial">
+  {reportsInProgress}
+</text>
+ <text x="220" y="183" text-anchor="end" fill="#00ffa6" font-size="13" font-weight="bold" font-family="Arial">
+  {reportsAccepted}
+</text>
   <text x="220" y="203" text-anchor="end" fill="#00ffa6" font-size="13" font-weight="bold" font-family="Arial">
-  {crReportsCount}
+  {reportsInformational}
 </text>
   <text x="220" y="223" text-anchor="end" fill="#00ffa6" font-size="13" font-weight="bold" font-family="Arial">
-  {crVulnerability}
+  {reportsDuplicate}
 </text>
   <text x="220" y="243" text-anchor="end" fill="#00ffa6" font-size="13" font-weight="bold" font-family="Arial">
-  {crBusinessRisk}
+  {reportsOutOfScope}
 </text>
-
+  <text x="220" y="263" text-anchor="end" fill="#00ffa6" font-size="13" font-weight="bold" font-family="Arial">
+  {reportsRejected}
+</text>
+  <text x="220" y="283" text-anchor="end" fill="#00ffa6" font-size="13" font-weight="bold" font-family="Arial">
+  {reportsAbuse}
+</text>
 </svg> 
-
 
     '''
     return bbsvg_template
